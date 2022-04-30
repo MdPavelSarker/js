377b6294db5ed5ede4483e008626bda259fcde8a
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-04-30 23:27:24 +0600

    math pratice

---
+++ math.js
@@ -0,0 +1,39 @@
+var price1 = 25;
+var price2 = 39;
+var total = price2 % price1;
+console.log(total);
+
+
+// price2 = price2 +1;
+price2++;
+console.log(price2);
+
+
+var price = 39;
+var name = '39';
+var count = name + ' ' + price;
+console.log(count);
+
+// Absoulate Number
+
+var number = -39;
+var absoulateNumber = Math.abs(number);
+console.log(absoulateNumber);
+
+
+var number = 39.4999;
+var result = Math.round(number);
+console.log(result);
+
+
+var number = 38.099;
+// var result = Math.ceil(number); Out put will be 39 
+var result = Math.floor(number);
+console.log(result);
+
+
+//  Lotery Random Numer 
+var number = Math.random()*100;
+var result = Math.round(number);
+console.log(result);
+
