1cc875030271c01e1e8a15896b56bdb3a294e568
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-01 22:36:29 +0600

    array work

---
+++ array1.js
@@ -0,0 +1,20 @@
+var teaLine = ['Abul', 'Haiul', 'Habul', 'Kabul','Gelam','Khailam'];
+
+var part = teaLine.slice(2 , 5);
+console.log(part);
+
+
+teaLine.push('Malam');
+console.log(teaLine);  
+
+
+teaLine.pop(teaLine);
+console.log(teaLine);
+
+
+teaLine.unshift('nasir','sajib');
+console.log(teaLine);
+
+
+teaLine.shift();
+console.log(teaLine); 
