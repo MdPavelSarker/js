43c9ddfbb48af8a3fcb8e76329f0f5be5c0b9965
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-02 02:46:40 +0600

    forloop

---
+++ forloop.js
@@ -0,0 +1,13 @@
+var nums = [55, 66, 77 ,81 , 87, 89, 93, 99, 10, 39,  47];
+
+for( var i = 0; i < nums.length ; i++){
+    var element = nums[i];
+    console.log(element);
+}
+
+
+
+// for( var i  = 1 ; i < 15; i++){
+//     console.log(i);
+
+// }
