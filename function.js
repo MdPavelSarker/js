c4fb55dc518775af6876c4a94382141fc20b511f
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-02 02:48:16 +0600

    function

---
+++ function.js
@@ -0,0 +1,35 @@
+// doubleIT used for yearlyBonus
+/** For multiple purpose*/
+function doubleIT(num){
+    var result =  num * 2;
+    return result;
+
+}
+var first = doubleIT(5);
+var second = doubleIT(15);
+var total = first + second;
+
+//console.log(total);
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
+
+}
+var sum = add(15,39);
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+// function sayHI(){
+//     console.log('Hello Function')
+//     console.log('Billy Chana ... Koi tumi')
+// }
+// sayHI();
