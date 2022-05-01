3ee6aadda6c2e3f6ff7f1b5cb633c18abf38e612
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-01 22:11:42 +0600

    array

---
+++ array.js
@@ -0,0 +1,23 @@
+var friendsAge = [15,17,19,21,23,25,27,29,31,33,35,37,39];
+
+
+var bedenaAge = friendsAge[5];
+
+
+friendsAge[9] = 41; 
+
+
+var position =  friendsAge.indexOf(39);
+
+
+friendsAge.push(08);
+friendsAge.push(09);
+
+
+console.log(friendsAge.length);
+
+
+friendsAge.pop();
+console.log(friendsAge);
+
+
