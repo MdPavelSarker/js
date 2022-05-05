9268efb273faa96da81ebd62d167bd788f0ab602
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-06 00:07:18 +0600

    FeetToInch Convert

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,1 +1,14 @@
-var inch =  156;
+// var inch =  156;
+// var feet = inch/12;
+// console.log(feet);
+
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var someOneFeet = inchToFeet(156);
+console.log(someOneFeet);
+var someOneFeet1 = inchToFeet(288);
+console.log(someOneFeet1);
+var someOneFeet2 = inchToFeet(300);
+console.log(someOneFeet2);
