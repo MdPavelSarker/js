5daca4869a482ad1d4351bfc5282f903f904bedf
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-06 23:55:55 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,18 @@
+function factorial(n){
+    var fact = 1;
+    for( var i = 1 ; i <= n; i++ ){
+        fact = fact * i;
+
+    }
+    return fact;
+}
+
+var result = factorial(10);
+console.log(result);
+
+
+// var factorial = 1;
+// for (var i = 1 ; i <= 10 ; i ++){
+//    factorial =  factorial * i 
+//     console.log(i , factorial);
+// }
