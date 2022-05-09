38313dcef350c8b147c2c85d562c1acf80b3019d
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-10 00:34:59 +0600

    function argument

---
+++ arguments.js
@@ -0,0 +1,27 @@
+function addNumbers(num1 , num2){
+    var sum = 0;
+    for (let i = 0; i <arguments.length; i++ ){
+        const num = arguments[i];
+        sum = sum + num;
+    }
+    return sum;
+
+}
+var result = addNumbers(3, 5, 8, 15, 22, 10);
+console.log('Total Number is:' , result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
