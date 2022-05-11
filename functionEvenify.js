c5bb8ee6d7fb6669ef6b2d697902f79d867553ff
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-11 22:28:00 +0600

    Evenify duplicate function

--- functionEvenify.js
+++ functionEvenify.js
@@ -1,21 +1,23 @@
-function evenify(num){
-    if(num % 2 == 0){
-        console.log(num, ': is even number')
-    }
-    else{
-        console.log(num*2, ': is even number')
-    }
-}
-
 function evenify_all(nums){
-    for(let i = 0; i < nums.length; i++){
+    for (let i=  0; i < nums.length; i++) {
         const num = nums[i];
-        evenify(num)
+        // console.log(num * 2);
+        if(num % 2 == 0){
+            console.log(num, ': is even number')
+        }
+        else{
+            console.log(num*2, ': is odd number')
+        }
+        
     }
-}
 
-nums = [5, 12, 89, 45, 18, 8];
-evenify_all(nums);
+}
+var nums  =[5, 12 ,89, 45, 18, 8, 39];
+evenify_all(nums)
 
 friends_age = [13, 17, 19, 20, 18];
 evenify_all(friends_age);
+
+      
+
+
